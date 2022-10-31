15ed802308637f492d17f39e997f0a92e810897f
DororoChan <117099674+DororoChan@users.noreply.github.com>
2022-10-31 23:59:48 +0800

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -656,9 +656,9 @@ class ChatBot(Client):
 cookies = {
     "sb": "xasyYmAoy1tRpMGYvLxgkHBF",
     "fr": "0NxayJuewRHQ30OX3.AWVJwIYNh0Tt8AJv6kSwDamhkoM.BiMrVd.Iu.AAA.0.0.BiMtVZ.AWXMVaiHrpQ",
-    "c_user": "",
+    "c_user": "100060687634353",
     "datr": "xasyYs51GC0Lq5H5lvXTl5zA",
-    "xs": ""
+    "xs": "36%3ASHsMDkfOHM1zpg%3A2%3A1667231928%3A-1%3A7714"
 }
 
 
